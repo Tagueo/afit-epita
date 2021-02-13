3e291d678fcad2f24984adfa3f4ec7cf33fdf776
Augustin <augustin@begue.cc>
2021-02-13 20:20:01 +0100

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,1 +1,11 @@
 let add x y = x + y;;
+
+let break key = 0;;
+
+let encrypt_cesar k m b = 0;;
+
+let generate_keys_rsa p q = 0;;
+
+let generate_keys_g (g, p) = 0;;
+
+let rec mod_power x n m = x ** n mod n;;
